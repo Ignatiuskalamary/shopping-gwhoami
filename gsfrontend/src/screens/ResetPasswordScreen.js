da76ca5efc8bb906a3e7b5801a3a8113937394af
Ignatiuskalamary <ignatius.n@gmail.com>
2023-08-29 17:30:10 -0400

    Shop V1.1

--- gsfrontend/src/screens/ResetPasswordScreen.js
+++ gsfrontend/src/screens/ResetPasswordScreen.js
@@ -1,42 +1,42 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-import { toast } from '../../node_modules/react-toastify/dist/index';
-import Axios from '../../node_modules/axios/index';
+// import React, { useEffect } from 'react'
+// import { useParams } from 'react-router-dom';
+// import { toast } from '../../node_modules/react-toastify/dist/index';
+// import Axios from '../../node_modules/axios/index';
 
-export default function ResetPasswordScreen()
-{
-    const navigate = useNavigate();
-    const { token } = useParams();
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+// export default function ResetPasswordScreen()
+// {
+//     const navigate = useNavigate();
+//     const { token } = useParams();
+//     const [password, setPassword] = useState('');
+//     const [confirmPassword, setConfirmPassword] = useState('');
 
-    const { state } = useContext(store);
-    const { userInfo } = state;
+//     const { state } = useContext(store);
+//     const { userInfo } = state;
 
-    useEffect(() =>
-    {
-        if (userInfo || !token)
-        {
-            navigate('/');
-        }
-    }, [navigate, userInfo, token]);
+//     useEffect(() =>
+//     {
+//         if (userInfo || !token)
+//         {
+//             navigate('/');
+//         }
+//     }, [navigate, userInfo, token]);
 
-    const submitHandler = async (e) =>
-    {
-        e.preventDefault();
-        if (password !== confirmPassword)
-        {
-            toast.error('Password do not match');
-            return;
-        }
-        try
-        {
-            await Axios.post('api/users/reset-ps')
-        }
-    }
-    return (
-        <div>
+//     const submitHandler = async (e) =>
+//     {
+//         e.preventDefault();
+//         if (password !== confirmPassword)
+//         {
+//             toast.error('Password do not match');
+//             return;
+//         }
+//         try
+//         {
+//             await Axios.post('api/users/reset-ps')
+//         }
+//     }
+//     return (
+//         <div>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
